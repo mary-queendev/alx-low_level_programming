e1694026b70865190baa593225e76c30ff4c98dc
Mary <ogbeidemary15@gmail.com>
2023-02-23 04:58:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-	/**
-	 * main - a program that prints _putchar, followed by a new line.
-	 * Description - prints _putchar
-	 * Return: 0(success)
-	 */
+/**
+ * main - print _putchar
+ * is written by mary
+ * Return: 0
+ */
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
